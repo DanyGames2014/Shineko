cd21f8f84f93871bcac5c6c0d8faf8a885be2e1d
Daniel Fildán <fildand@centrum.cz>
2026-07-04 21:16:57 +0200

    Seems to be worse

--- src/main/java/net/danygames2014/shineko/mixin/WorldRendererMixin.java
+++ src/main/java/net/danygames2014/shineko/mixin/WorldRendererMixin.java
@@ -4,6 +4,7 @@ import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.client.render.WorldRenderer;
 import net.minecraft.client.render.chunk.ChunkBuilder;
+import net.minecraft.entity.Entity;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -17,5 +18,14 @@ public class WorldRendererMixin {
         }
         
         return original.call(chunkBuilder);
+    }
+    
+    @WrapOperation(method = "compileChunks", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/chunk/ChunkBuilder;squaredDistanceTo(Lnet/minecraft/entity/Entity;)F", ordinal = 1))
+    public float fixRaceCondition2(ChunkBuilder chunkBuilder, Entity entity, Operation<Float> original) {
+        if (chunkBuilder == null) {
+            return 257.0F;
+        }
+        
+        return original.call(chunkBuilder, entity);
     }
 }
